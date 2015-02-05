1f12fddab5445e05de27ebd2044787ffbc9711a9
root <root@itatests.com>
2015-02-05 23:19:13 +0000

    jagdfksf

--- dating/index.php
+++ dating/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // change the following paths if necessary
-$yii='/opt/yii/framework/yii.php';
+$yii=dirname(__FILE__).'/../Downloads/yii-1.1.16.bca042/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
