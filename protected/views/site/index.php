ac69fc9b8ab16b03bc05d01ff6a17aa57d034de4
Oleksii Tiurenkov <alterego4@gmail.com>
2015-02-06 00:49:37 +0200

    no message

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,7 +1,7 @@
 
 <?php 
 echo CHtml::dropDownList('city', NULL, $citiesArray);
-echo CHtml::br;
+echo CHtml::tag('br');
 echo CHtml::dropDownList('category', NULL, $categoriesArray);
 echo CHtml::br;
 echo CHtml::dropDownList('target', NULL, $targetsArray);
