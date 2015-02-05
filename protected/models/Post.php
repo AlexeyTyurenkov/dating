bfa8613fcc9df99077c8f62830798ab9a7670a7a
root <root@itatests.com>
2015-02-05 22:01:36 +0000

    models

---
+++ protected/models/Post.php
@@ -0,0 +1,114 @@
+<?php
+
+/**
+ * This is the model class for table "post".
+ *
+ * The followings are the available columns in table 'post':
+ * @property string $id
+ * @property string $user_id
+ * @property string $create_date
+ * @property string $city_id
+ * @property string $category_id
+ * @property string $target_id
+ * @property string $header
+ * @property string $text
+ */
+class Post extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'post';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('user_id, create_date, city_id, category_id, target_id, header, text', 'required'),
+			array('user_id, city_id, category_id, target_id', 'length', 'max'=>10),
+			array('header', 'length', 'max'=>80),
+			array('text', 'length', 'max'=>2000),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, user_id, create_date, city_id, category_id, target_id, header, text', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'user_id' => 'User',
+			'create_date' => 'Create Date',
+			'city_id' => 'City',
+			'category_id' => 'Category',
+			'target_id' => 'Target',
+			'header' => 'Header',
+			'text' => 'Text',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id,true);
+		$criteria->compare('user_id',$this->user_id,true);
+		$criteria->compare('create_date',$this->create_date,true);
+		$criteria->compare('city_id',$this->city_id,true);
+		$criteria->compare('category_id',$this->category_id,true);
+		$criteria->compare('target_id',$this->target_id,true);
+		$criteria->compare('header',$this->header,true);
+		$criteria->compare('text',$this->text,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return Post the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
