eb5d61bc59e7819d7440818ad6336a7dc41ae5af
Oleksii Tiurenkov <alterego4@gmail.com>
2015-02-08 15:55:40 +0200

    no message

--- protected/views/site/_smallPost.php
+++ protected/views/site/_smallPost.php
@@ -1,8 +1,10 @@
-<?php
 
-echo $model->category->name;
-echo $model->target->name;
-echo $model->city->name;
-echo $model->header;
+<div class="smallpost">
+    <div class="smallpostline city"><?php echo $model->city->name; ?></div>
+    <div class="smallpostline age"><?php echo $model->age?></div>
+    <div class="smallpostline category"><?php echo $model->category->name;?></div>
+    <div class="smallpostline target"><?php echo $model->target->name; ?></div>
+    <div class="smallpostline header"><?php echo $model->header; ?></div>
+</div>
 
-?>
+
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -2,13 +2,13 @@
 <?php 
 echo CHtml::beginForm();
 echo CHtml::tag('span');
-echo CHtml::dropDownList('city', $cityPreselected, CHtml::listData(City::model()->findAll(), 'id', 'name'),array('empty'=>"Выберите город...", 'class' => "bigSelector"));
+echo CHtml::dropDownList('city', $cityPreselected, CHtml::listData(City::model()->findAll(), 'id', 'name'),array('empty'=>"Все города", 'class' => "bigSelector"));
 echo CHtml::tag('/span');
 echo CHtml::tag('span');
-echo CHtml::dropDownList('category', NULL, CHtml::listData(Category::model()->findAll(), 'id', 'name'),array('empty'=>"Выберите цель...", 'class' => "bigSelector"));
+echo CHtml::dropDownList('category', $categoryPreselected, CHtml::listData(Category::model()->findAll(), 'id', 'name'),array('empty'=>"Все цели", 'class' => "bigSelector"));
 echo CHtml::tag('/span');
 echo CHtml::tag('span');
-echo CHtml::dropDownList('target', NULL, CHtml::listData(Target::model()->findAll(), 'id', 'name'),array('empty'=>"Выберите категорию...", 'class' => "bigSelector"));
+echo CHtml::dropDownList('target', $targetPreselected, CHtml::listData(Target::model()->findAll(), 'id', 'name'),array('empty'=>"Все категории", 'class' => "bigSelector"));
 echo CHtml::tag('/span');
 echo CHtml::submitButton('Фильтр');
 echo CHtml::link("Добавить объявление", array('addNew'));
