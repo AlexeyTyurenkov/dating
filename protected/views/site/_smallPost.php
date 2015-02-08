29153b7fa706358414bd15e2d481aeefe1666363
Oleksii Tiurenkov <alterego4@gmail.com>
2015-02-08 18:52:55 +0200

    no message

--- protected/views/site/_smallPost.php
+++ protected/views/site/_smallPost.php
@@ -1,7 +1,6 @@
 
 <div class="smallpost">
     <div class="smallpostline city"><?php echo $model->city->name; ?></div>
-    <div class="smallpostline emptyspace">&nbsp;</div>
     <div class="smallpostline age"><?php echo $model->age?></div>
     <div class="smallpostline category"><?php echo $model->category->name;?></div>
     <div class="smallpostline target"><?php echo $model->target->name; ?></div>
