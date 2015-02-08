cf77f7c84f2b13ee50e9de34e47fa4a47220d326
Oleksii Tiurenkov <alterego4@gmail.com>
2015-02-08 19:33:05 +0200

    no message

--- protected/views/site/_smallPost.php
+++ protected/views/site/_smallPost.php
@@ -3,6 +3,7 @@
     <div class="smallpostline"><span><?php echo $model->city->name; ?></span><span class="right"><?php echo $model->age; ?></span></div>
     <div class="smallpostline"><span><?php echo $model->category->name; ?></span><span class="right"><?php echo $model->target->name; ?></span></div>
     <div class="smallpostline header"><?php echo $model->header; ?></div>
+    <div class="smallpostline header"><?php echo CHtml::link($model->header, Yii::app()->createUrl("site/showPost", array('id'=>$model->id)))?></div>
 </div>
 
 
