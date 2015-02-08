487f96034b289dcd3a825cb3d3dca3e1b8c46f46
Oleksii Tiurenkov <alterego4@gmail.com>
2015-02-08 19:24:25 +0200

    no message

--- protected/views/site/_smallPost.php
+++ protected/views/site/_smallPost.php
@@ -1,7 +1,7 @@
 
 <div class="smallpost">
-    <div class="smallpostline"><span class="left"><?php echo $model->city->name; ?></span><span class="right"><?php echo $model->age; ?></span></div>
-    <div class="smallpostline"><span class="left"><?php echo $model->category->name; ?></span><span class="right"><?php echo $model->target->name; ?></span></div>
+    <div class="smallpostline"><span><?php echo $model->city->name; ?></span><span class="right"><?php echo $model->age; ?></span></div>
+    <div class="smallpostline"><span><?php echo $model->category->name; ?></span><span class="right"><?php echo $model->target->name; ?></span></div>
     <div class="smallpostline header"><?php echo $model->header; ?></div>
 </div>
 
