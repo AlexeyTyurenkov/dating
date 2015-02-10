e4b17fdc733efde4fceb1a3c91b9ad543dc58882
Oleksii Tiurenkov <alterego4@gmail.com>
2015-02-10 19:21:33 +0200

    added fields to filter

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,5 +1,11 @@
+<div style="text-align: center; width:40%;">
+    <?php echo CHtml::link("Добавить объявление", array('addNew'));?>
+    
+</div>
+
 <div class="foriframe">
-<?php 
+<?php
+echo CHtml::link("Добавить объявление", array('addNew'));
 echo CHtml::beginForm();
 echo CHtml::tag('span');
 echo CHtml::dropDownList('city', $cityPreselected, CHtml::listData(City::model()->findAll(), 'id', 'name'),array('empty'=>"Все города", 'class' => "bigSelector"));
@@ -10,8 +16,12 @@ echo CHtml::tag('/span');
 echo CHtml::tag('span');
 echo CHtml::dropDownList('target', $targetPreselected, CHtml::listData(Target::model()->findAll(), 'id', 'name'),array('empty'=>"Все категории", 'class' => "bigSelector"));
 echo CHtml::tag('/span');
+echo "Возраст от:";
+echo CHtml::textField('minAge');
+echo "до:";
+echo CHtml::textField('maxAge');
 echo CHtml::submitButton('Фильтр');
-echo CHtml::link("Добавить объявление", array('addNew'));
+
 echo CHtml::endForm();
 
 ?>
