60330593e4fec6e82d7aa094e4a51214095ac784
Oleksii Tiurenkov <alterego4@gmail.com>
2015-05-30 20:46:12 +0300

    no message

--- protected/views/site/_fullList.php
+++ protected/views/site/_fullList.php
@@ -6,7 +6,8 @@ $this->widget('zii.widgets.CListView', array(
     'itemView'=>'_smallPost',
     'summaryText' => "<div class='summaryText'><p class='summaryPages'>страница <span class='pageCurrent'>{start}</span> / <span class='pageEnd'>{pages}</span></p><p class='summaryResults'>всего {count} анкет</p></div><a class='summaryAdd' href='".Yii::app()->createUrl("site/add")."'><span>+</span> добавить свою</a>",
     'template'=>'{items}<div class="loadItems"></div><div class="clearfix"></div>{summary}<div class="clearfix"></div><div class="loadPage"></div>',
-    'id' => 'PostsList'
+    'id' => 'PostsList',
+    'emptyText'=> "Пока нет объявлений. Будьте <a class='text-link' href='/addNew'> первым </a>."
 ));
 ?>
 <script type="text/javascript">
